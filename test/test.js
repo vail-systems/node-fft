a77522e362a7cad024cfc72fa83daaaaf76c566c
Ben Bryan <bbryan@vailsys.com>
2015-07-02 13:53:35 -0500

    Add tests for random vector.

--- test/test.js
+++ test/test.js
@@ -3,14 +3,14 @@ var assert = require('assert'),
     dft = require('../').dft;
 
 describe('FFT (Cooley-Tukey)', function () {
+
     describe('1,0,1,0', function () {
         it('Should properly compute [1,0,1,0]', function () {
             var coef = fft([1,0,1,0]);
-            console.log(coef);
             checkShortVectorWithThresh(coef);
         });
-    }); 
-    
+    });
+
     describe('1,0,1,0,2,0,2,0', function () {
         it('Should properly compute [1,0,1,0,2,0,2,0]', function () {
             var coef = fft([1,0,1,0,2,0,2,0]);
@@ -23,11 +23,50 @@ describe('DFT O(n^2) Brute Force', function() {
    describe('1,0,1,0', function() {
       it('Should properly compute [1, 0, 1, 0]', function() {
           var coef = dft([1,0,1,0]);
-          console.log(coef);
           checkShortVectorWithThresh(coef);
       });
    });
 });
+
+describe('Compare FFT to DFT', function() {
+    var randomSignal = signalVector();
+    var coefFFT, coefDFT;
+
+    describe('randomSignal FFT', function() {
+        it ('Should compute randomSignal', function() {
+            coefFFT = fft(randomSignal);
+            assert(coefFFT && coefFFT.length == randomSignal.length);
+        });
+    });
+    describe('randomSignal DFT', function() {
+        it('Should compute randomSignal', function() {
+            coefDFT = dft(randomSignal);
+            assert(coefDFT && coefDFT.length == randomSignal.length);
+        })
+    });
+    describe('randomSignal FFT and DFT', function() {
+        it('Should compute same output', function() {
+
+            //Loop over all elements in the two output arrays
+            for (var i = 0; i < randomSignal.length; i++) {
+                //Check that they are equal within reason
+                assert(equalWithThresh(coefFFT[i][0], coefDFT[i][0], 0.01));
+                assert(equalWithThresh(coefFFT[i][1], coefDFT[i][1], 0.01));
+            }
+        })
+    });
+});
+
+function signalVector(vectorLength) {
+  vectorLength = vectorLength || 16;
+  var vector = [];
+
+  for (var i = 0; i < vectorLength; i++) {
+    vector[i] = Math.random();
+  }
+
+  return vector;
+};
 
 function checkShortVectorWithThresh(coef) {
     assert(equalWithThresh(coef[0][0], 2, 0.01));
@@ -60,6 +99,6 @@ function checkLongVectorWithThresh(coef) {
 }
 
 function equalWithThresh(val1, val2, threshold) {
-    return (val1 > val2 - threshold) && 
+    return (val1 > val2 - threshold) &&
            (val1 < val2 + threshold);
 }

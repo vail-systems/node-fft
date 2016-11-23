35034f529b17dcdc3d642b7b2ce7a2df2497169e
Maximilian Bügler <max@maxbuegler.eu>
2016-11-23 21:55:05 +0100

    Added header

--- src/ifft.js
+++ src/ifft.js
@@ -1,9 +1,32 @@
-var fft = require('fft-js').fft;
+/*===========================================================================*\
+ * Inverse Fast Fourier Transform (Cooley-Tukey Method)
+ *
+ * (c) Maximilian Bügler. 2016
+ *
+ * Based on and using the code by
+ * (c) Vail Systems. Joshua Jung and Ben Bryan. 2015
+ *
+ * This code is not designed to be highly optimized but as an educational
+ * tool to understand the Fast Fourier Transform.
+\*===========================================================================*/
+
+//------------------------------------------------
+// Note: Some of this code is not optimized and is
+// primarily designed as an educational and testing
+// tool.
+// To get high performace would require transforming
+// the recursive calls into a loop and then loop
+// unrolling. All of this is best accomplished
+// in C or assembly.
+//-------------------------------------------------
 
 //-------------------------------------------------
 // The following code assumes a complex number is
 // an array: [real, imaginary]
 //-------------------------------------------------
+
+var fft = require('./fft').fft;
+
 
 module.exports = {
     ifft: function ifft(signal){
